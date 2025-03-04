b20638dcd044314615a3574b3403c75196e129bd
AlexandrKhromov2005 <146848229+AlexandrKhromov2005@users.noreply.github.com>
2025-03-04 09:03:38 +0300

    Update POB.h

--- HT_GBO/src/POB.h
+++ HT_GBO/src/POB.h
@@ -4,9 +4,7 @@
 #include <vector>
 #include <utility>
 
-// Кодирование 4-битного числа в POB
 std::pair<int, int> pob(int val);
-// Декодирование POB в 4-битное число
 int inverse_pob(int target_pob, int r);
 
 #endif // POB_H
