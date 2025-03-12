f8514e6bc604b8a5feaba7c5e62656b6321b580b
Tagir <146005895+BioGD@users.noreply.github.com>
2025-03-12 16:30:09 +0300

    Update Hadamard.cpp

--- HT_GBO/src/Hadamard.cpp
+++ HT_GBO/src/Hadamard.cpp
@@ -2,36 +2,39 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-// Преобразование Адамара для матрицы 4x4
+
 void Hadamard::applyHadamard(cv::Mat& block) {
-    // Матрица Адамара размерности 4x4
+    cv::Mat block_d;
+    block.convertTo(block_d, CV_64F); 
+
     cv::Mat H4 = (cv::Mat_<double>(4, 4) <<
         1, 1, 1, 1,
         1, -1, 1, -1,
         1, 1, -1, -1,
         1, -1, -1, 1) / 2.0;
 
-    // Умножаем блок на матрицу Адамара
-    block = H4 * block;
+    cv::Mat transformed = H4 * block_d * H4.t();
+
+    block = transformed.clone(); 
 }
 
-// Умножение матрицы 4x4 на матрицу Адамара
-cv::Mat Hadamard::multiplyWithHadamard(cv::Mat& input) {
-    cv::Mat result = input.clone();
-    applyHadamard(result);
-    return result;
-}
-
-// Обратное преобразование Адамара для матрицы 4x4
 void Hadamard::applyInverseHadamard(cv::Mat& block) {
-    // Транспонированная матрица Адамара для 4x4
-    cv::Mat H4_T = (cv::Mat_<double>(4, 4) <<
+    cv::Mat block_d;
+    block.convertTo(block_d, CV_64F); 
+
+    cv::Mat H4 = (cv::Mat_<double>(4, 4) <<
         1, 1, 1, 1,
         1, -1, 1, -1,
         1, 1, -1, -1,
         1, -1, -1, 1) / 2.0;
 
-    // Умножаем на транспонированную матрицу Адамара (обратное преобразование)
-    block = H4_T * block;
+    cv::Mat transformed = H4 * block_d * H4.t();
+
+    
+    block = transformed.clone();
+    block.forEach<double>([](double& pixel, const int* position) {
+        pixel = cv::saturate_cast<uchar>(std::round(pixel));
+        });
 }
+
 
